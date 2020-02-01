dd78f62561b9ff6831abf46c6b2c9d3e7bdf8775
pyavchik <pyavchik@gmail.com>
2020-02-01 19:12:15 +0200

    added date and currency pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name: string;
+  date: string;
+  amount: number;
+
   onNameChange(value: string) {
     this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
   }
 }
